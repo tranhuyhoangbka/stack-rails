7c02f23f71a39f0e5b3f9af827adc5514aff0c19
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-05-23 00:31:49 +0700

    ok

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,13 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = params[:page].to_i || 0
     @customers = if params[:keywords].present?
-      @keywords = params[:keywords]
-      customer_search_term = CustomerSearchTerm.new @keywords
-      Customer.search customer_search_term.where_clause,
-        customer_search_term.where_args, customer_search_term.order
+      customer_search_term = CustomerSearchTerm.new params[:keywords]
+      Customer.search(customer_search_term.where_clause,
+        customer_search_term.where_args, customer_search_term.order)
+        .offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       []
     end
