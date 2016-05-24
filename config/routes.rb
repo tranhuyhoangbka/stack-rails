1d4ffa440dfa250e188277ded4675165595d0bb9
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-05-25 01:41:06 +0700

    first for angular

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
   devise_for :users
   root "dashboard#index"
+  get "angular_test", to: "angular_test#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
